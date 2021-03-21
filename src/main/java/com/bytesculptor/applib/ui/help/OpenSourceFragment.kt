aedae2faa54b5ee78834b934be3cf40308a8c8d8
Byte_Sculptor <bytesculptor@gmail.com>
2021-03-21 14:58:52 +0000

    conversion to Kotlin

--- src/main/java/com/bytesculptor/applib/ui/help/OpenSourceFragment.kt
+++ src/main/java/com/bytesculptor/applib/ui/help/OpenSourceFragment.kt
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2017 - 2021  Byte Sculptor Software
+ * Copyright (c) 2020 Byte Sculptor Software
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -21,31 +21,17 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+package com.bytesculptor.applib.ui.help
 
-package com.bytesculptor.applib.ui.help;
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.bytesculptor.applib.R
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import androidx.fragment.app.Fragment;
-
-import com.bytesculptor.applib.R;
-import com.bytesculptor.applib.utilities.ExternalLinksHelper;
-
-
-public class DisclaimerFragment extends Fragment {
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_disclaimer, container, false);
-        TextView feedback = view.findViewById(R.id.tvFeedbackMailLink);
-        if (feedback != null) {
-            feedback.setOnClickListener(v -> ExternalLinksHelper.sendFeedbackMail(requireContext(), "App"));
-        }
-        return view.getRootView();
+class OpenSourceFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_open_source, container, false)
     }
-
 }
