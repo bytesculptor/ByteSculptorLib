630f31323130f465b33e0bef32b43498341eda53
Byte_Sculptor <bytesculptor@gmail.com>
2021-10-03 11:32:56 +0100

    Theme constants / array moved to BSS lib

---
+++ src/main/java/com/bytesculptor/applib/utilities/Constants.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021 Byte Sculptor Software - All Rights Reserved
+ *
+ * All information contained herein is and remains the property of Byte Sculptor Software.
+ * Unauthorized copying of this file, via any medium, is strictly prohibited unless prior
+ * written permission is obtained from Byte Sculptor Software.
+ *
+ * Romeo Rondinelli - bytesculptor@gmail.com
+ *
+ */
+
+package com.bytesculptor.applib.utilities;
+
+public class Constants {
+
+    public static final int THEME_LIGHT = 0;
+    public static final int THEME_DARK = 1;
+    public static final int THEME_SYSTEM = 2;
+
+    public static final String KEY_PREF_THEME = "KEY_PREF_THEME";
+    public static final String DEFAULT_THEME = String.valueOf(THEME_LIGHT);
+}
