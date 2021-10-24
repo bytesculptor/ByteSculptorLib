c1affec814a31209b672e242ac647da6c0ad9568
Byte_Sculptor <bytesculptor@gmail.com>
2021-10-24 21:52:19 +0100

    Bugfix double call and outside try/catch

--- src/main/java/com/bytesculptor/applib/utilities/ExternalLinksHelper.java
+++ src/main/java/com/bytesculptor/applib/utilities/ExternalLinksHelper.java
@@ -71,7 +71,6 @@ public class ExternalLinksHelper {
         goToAppStore.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
                 Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
                 Intent.FLAG_ACTIVITY_MULTIPLE_TASK | FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(goToAppStore);
         try {
             context.startActivity(goToAppStore);
         } catch (
