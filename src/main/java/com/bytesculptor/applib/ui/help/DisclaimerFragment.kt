d8067983befd37b1cdc505834d81ff831cf13971
Byte Sculptor <bytesculptor@gmail.com>
2023-07-25 21:16:11 +0100

    Refactoring

--- src/main/java/com/bytesculptor/applib/ui/help/DisclaimerFragment.kt
+++ src/main/java/com/bytesculptor/applib/ui/help/DisclaimerFragment.kt
@@ -18,25 +18,17 @@
 package com.bytesculptor.applib.ui.help
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
+import android.view.*
 import androidx.fragment.app.Fragment
 import com.bytesculptor.applib.R
-import com.bytesculptor.applib.utilities.ExternalLinksHelper
 
 class DisclaimerFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val view = inflater.inflate(R.layout.fragment_disclaimer, container, false)
-        val feedback = view.findViewById<TextView>(R.id.tvFeedbackMailLink)
-        feedback?.setOnClickListener { _: View? ->
-            ExternalLinksHelper.sendFeedbackMail(
-                requireContext(),
-                "App",
-                getString(R.string.no_email_app_found)
-            )
-        }
         return view.rootView
     }
 }
