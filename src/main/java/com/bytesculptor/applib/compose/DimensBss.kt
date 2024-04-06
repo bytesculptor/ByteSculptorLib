3d082c291dc0f9d0d6fd817775eb38c03a5a9fc7
Byte Sculptor <bytesculptor@gmail.com>
2024-04-06 09:44:02 +0200

    Introduce DimensBss.kt

---
+++ src/main/java/com/bytesculptor/applib/compose/DimensBss.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2024 Byte Sculptor Software
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.bytesculptor.applib.compose
+
+import androidx.compose.ui.unit.dp
+
+object DimensBss {
+
+    object Button {
+        val width = 150.dp
+        val height = 56.dp
+    }
+
+    object Spacer {
+        val XS = 4.dp
+        val S = 8.dp
+        val M = 16.dp
+        val L = 24.dp
+        val XL = 32.dp
+    }
+}
